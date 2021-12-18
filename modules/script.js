bb2145c8fe4598b322f40c6461072dcd3076913d
it-dude-ru <95648923+it-dude-ru@users.noreply.github.com>
2021-12-18 19:41:01 +0300

    test commit

--- modules/script.js
+++ modules/script.js
@@ -1,13 +1,21 @@
 import { Layer } from './layer.js'
 
+/**
+ * 
+ * 
+ */
 class App {
 	constructor(container) {
 		this.layer = new Layer(container);
 	}
 }
 
+// Just comment
+// ! Better comment
+// ? Best comment
+/**
+ * 
+ */
 onload = () => {
 	new App(document.querySelector('body'));
 }
-
-alert ('Hello world!');
